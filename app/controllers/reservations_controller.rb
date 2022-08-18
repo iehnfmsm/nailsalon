c8fa69f54f0ded80d3d0d1f6c0b0675d21dfe548
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-18 17:26:52 +0900

    希望日時読み取り確認

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,7 @@
 class ReservationsController < ApplicationController
   def index
-    
+    if user_signed_in?
+      @user_prefers = UserPrefer.where(user_id: current_user.id)
+    end
   end
 end
